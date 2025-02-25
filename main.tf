17c6d34b3862e1d6faaea095e5c50b78e040860f
nitheshsivakumar <nitheshsivakumar19@gmail.com>
2025-02-25 10:26:41 -0600

    created ecs, td, service

--- main.tf
+++ main.tf
@@ -95,3 +95,20 @@ module "ecs_task_execution_role" {
   env_file_bucket_name = module.s3_bucket.env_file_bucket_name
   environment          = local.environment
 }
+
+#create ecs cluster, task definition and service
+module "ecs" {
+  source                       = "git@github.com:nitheshsivakumar/terraform-modules.git//ecs"
+  project_name                 = local.project_name
+  environment                  = local.environment
+  ecs_task_execution_role_arn  = module.ecs_task_execution_role.ecs_task_execution_role_arn
+  architecture                 = var.architecture
+  container_image              = var.container_image
+  env_file_bucket_name         = module.s3_bucket.env_file_bucket_name
+  env_file_name                = module.s3_bucket.env_file_name
+  region                       = local.region
+  private_app_subnet_az1_id    = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id    = module.vpc.private_app_subnet_az2_id
+  app_server_security_group_id = module.security_group.app_server_security_group_id
+  alb_target_group_arn         = module.application_load_balancer.alb_target_group_arn
+}
